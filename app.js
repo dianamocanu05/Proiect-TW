cc3a3c4ae69547134eb1a5ef750e15f64fb12888
dianamocanu05 <diana.paula.mocanu@gmail.com>
2021-03-27 17:10:40 +0200

    solved smth

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const http = require('http');
-const port = process.env.PORT || 3000
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
     //res.statusCode = 200;
     res.setHeader('Access-Control-Allow-Origin', '*');
+
     const path = "D:\\UNI\\SEM_2\\Proiect TW\\HTML\\index.html";
     fs.readFile(path, function (err, html) {
         if (err) {
@@ -14,6 +15,4 @@ const server = http.createServer((req, res) => {
     });
 });
 
-server.listen(port,() => {
-    console.log(`Server running at port `+port);
-});
+server.listen(port);
