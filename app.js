ec9c115fb3f1857d26d0e0651edd8c1d584661f0
dianamocanu05 <diana.paula.mocanu@gmail.com>
2021-03-27 17:44:15 +0200

    solved smth

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const http = require('http');
 const fs = require('fs');
 
 const port = process.env.PORT || 3000;
-console.log(port);
 const server = http.createServer((req, res) => {
     //res.statusCode = 200;
     res.setHeader('Access-Control-Allow-Origin', '*');
