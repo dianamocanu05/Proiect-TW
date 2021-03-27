84696f6cf190b6c76721da6720a4cbf6c34c402a
dianamocanu05 <diana.paula.mocanu@gmail.com>
2021-03-27 18:42:32 +0200

    is it working yet?

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const http = require('http');
 const fs = require('fs');
 
 const port = process.env.PORT || 3000;
+console.log(process.env.PORT)
+console.log(port)
 const server = http.createServer((req, res) => {
     //res.statusCode = 200;
     res.setHeader('Access-Control-Allow-Origin', '*');
