841aec196f7b2006f611a4fe85d42949a4ce359b
dianamocanu05 <diana.paula.mocanu@gmail.com>
2021-03-27 15:55:47 +0200

    changed .js

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
+    const path = "/HTML/index.html";
+    fs.readFile(path, function (err, html) {
+        if (err) {
+            throw err;
+        }
+        res.end(html);
+    });
 });
 
 server.listen(port,() => {
