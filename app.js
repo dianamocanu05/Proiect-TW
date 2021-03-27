0065b798c215097008cceac48aeedf5c230c3c42
dianamocanu05 <diana.paula.mocanu@gmail.com>
2021-03-27 19:07:13 +0200

    still not working

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const server = http.createServer((req, res) => {
     //res.statusCode = 200;
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    const path = "D:\\UNI\\SEM_2\\Proiect TW\\HTML\\index.html";
+    const path = "./HTML/index.html";
     fs.readFile(path, function (err, html) {
         if (err) {
             throw err;
         }
-        res.end(html);
+        res.write(html);
+        res.end();
     });
 });
 
