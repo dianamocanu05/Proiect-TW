ff77cc073b14631078e46dc8fdb410ce9f7fe7bb
dianamocanu05 <diana.paula.mocanu@gmail.com>
2021-04-09 13:49:27 +0300

    commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const http = require('http');
 
 const sqlite3 = require("sqlite3");
-export {sqlite3}
+module.exports = {sqlite3}
 const port = process.env.PORT || 3000;
 
 //const port = 3000;
--- scripts/query_database.js
+++ scripts/query_database.js
@@ -1,21 +1,21 @@
 import {sqlite3} from "../app";
 
-let list = "aaza";
-// exports.select = function (cb){
-//     let db = sqlite3.Database("../data/mock/mock-db.sqlite3");
-//     db.all("SELECT * FROM accidents", function (err,rows){
-//         if(err) return cb(err);
-//         let count = 0;
-//         rows.forEach(function (row){
-//             console.log(row)
-//             list[count] = row;
-//         });
-//         db.close();
-//         return cb(null,list);
-//     });
-// }
+let list = [];
+exports.select = function (cb){
+    let db = sqlite3.Database("../data/mock/mock-db.sqlite3");
+    db.all("SELECT * FROM accidents", function (err,rows){
+        if(err) return cb(err);
+        let count = 0;
+        rows.forEach(function (row){
+            console.log(row)
+            list[count] = row;
+        });
+        db.close();
+        return cb(null,list);
+    });
+}
 
-export {list}
+module.exports  = {list}
 
 
 
