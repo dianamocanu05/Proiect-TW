f9c2148282dc4f61036231ff8f87c8803c921f63
dianamocanu05 <diana.paula.mocanu@gmail.com>
2021-04-09 14:36:43 +0300

    solved portgit add .git add .

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const fs = require('fs');
 const http = require('http');
 
-const sqlite3 = require("sqlite3");
-module.exports = {sqlite3}
+
+const {query} = require('./scripts/query_database')
 const port = process.env.PORT || 3000;
 
 //const port = 3000;
 const server = http.createServer((req, res) => {
     //res.statusCode = 200;
+    console.log(req.url)
     res.setHeader('Access-Control-Allow-Origin', '*');
+    if(req.url === "/"){
+        routing(res,"./html/index.html");
+        query()
+    }
+    else if(req.url !== "/favicon.ico"){
+        if(req.url === "/media-util/usa.svg"){
+            res.setHeader('Content-Type', 'image/svg+xml');
+        }
+        routing(res,`.${req.url}`);
+    }
 
-    const path = "./html/index.html";
+});
+
+function routing(res, path){
+
     fs.readFile(path, function (err, html) {
         if (err) {
             throw err;
@@ -18,8 +32,7 @@ const server = http.createServer((req, res) => {
         res.write(html);
         res.end();
     });
-});
-
+}
 server.listen(port, (err) => {
     if (err) {
         return console.error(err);
--- scripts/query_database.js
+++ scripts/query_database.js
@@ -1,21 +1,27 @@
-import {sqlite3} from "../app";
+const sqlite3 = require("sqlite3");
+resolve = require('path').resolve
 
-let list = [];
-exports.select = function (cb){
-    let db = sqlite3.Database("../data/mock/mock-db.sqlite3");
+
+function query(cb){
+    let db_path = resolve("data\\mock\\mock-db.sqlite3");
+    let list = [];
+    let db = new sqlite3.Database(db_path);
+    console.log(db_path)
+    console.log(resolve("."))
     db.all("SELECT * FROM accidents", function (err,rows){
         if(err) return cb(err);
         let count = 0;
         rows.forEach(function (row){
             console.log(row)
             list[count] = row;
+            count++
         });
         db.close();
-        return cb(null,list);
+        return list
     });
 }
 
-module.exports  = {list}
+module.exports  = {query}
 
 
 
