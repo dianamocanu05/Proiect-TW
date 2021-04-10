a89a2ea26dd24e1781d8862099e6f46bfb106628
olaruandreea12 <39705342+olaruandreea12@users.noreply.github.com>
2021-04-10 23:00:07 +0300

    table

---
+++ scripts/tabel.js
@@ -0,0 +1,28 @@
+google.charts.load('current', {'packages':['table']});
+google.charts.setOnLoadCallback(drawTable);
+
+function drawTable() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'ID');
+    data.addColumn('number', 'Severity');
+    data.addColumn('string', 'Start Time');
+    data.addColumn('string', 'Street');
+    data.addColumn('string', 'Side');
+    data.addColumn('string', 'City');
+    data.addColumn('string', 'County');
+    data.addColumn('string', 'State');
+    data.addColumn('number', 'Temperature');
+    data.addColumn('number', 'Visibility(mi)');
+    data.addColumn('string', 'Weather Condition');
+
+    data.addRows([
+        ['A-1',	3,	'08-02-16 5:46',	'I-70 E',	'R','Dayton',	'Montgomery', 'OH',	36.9,	10,	'Light Rain'],
+        ['A-2',	2,	'08-02-16 6:07',	'Brice Rd',	'L', 'Reynoldsburg','Franklin',	'OH', 37.9 ,10,	'Light Rain'],
+        ['A-3',	2,	'08-02-16 6:49',	'State Route 32',	'R',	'Williamsburg',	'Clermont',	'OH',	36,	10,	'Overcast'],
+        ['A-4',	3,	'08-02-16 7:23',	'I-75 S',	'R',	'Dayton',	'Montgomery',	'OH',35.1,	9,	'Mostly Cloudy']
+    ]);
+
+    var table = new google.visualization.Table(document.getElementById('table_div'));
+
+    table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+}
