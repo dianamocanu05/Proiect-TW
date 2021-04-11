ee0f0c68348c428b340da9d252d9d1cfa91df0f8
olaruandreea12 <39705342+olaruandreea12@users.noreply.github.com>
2021-04-11 21:31:30 +0300

    date tabel

--- scripts/tabel.js
+++ scripts/tabel.js
@@ -21,11 +21,11 @@ function drawTable() {
         ['A-3',	2,	'08-02-16 6:49',	'State Route 32',	'R',	'Williamsburg',	'Clermont',	'OH',	36,	10,	'Overcast'],
         ['A-4',	3,	'08-02-16 7:23',	'I-75 S',	'R',	'Dayton',	'Montgomery',	'OH',35.1,	9,	'Mostly Cloudy'],
         ['A-5',	2,	'08-02-16 7:39',	'Miamisburg Centerville Rd',	'R', 'Dayton',	'Montgomery',	'OH',	36,	6,	'Mostly Cloudy'],
-        ['A-4',	3,	'08-02-16 7:23',	'I-75 S',	'R',	'Dayton',	'Montgomery',	'OH',35.1,	9,	'Mostly Cloudy'],
-        ['A-4',	3,	'08-02-16 7:23',	'I-75 S',	'R',	'Dayton',	'Montgomery',	'OH',35.1,	9,	'Mostly Cloudy'],
-        ['A-4',	3,	'08-02-16 7:23',	'I-75 S',	'R',	'Dayton',	'Montgomery',	'OH',35.1,	9,	'Mostly Cloudy'],
-        ['A-4',	3,	'08-02-16 7:23',	'I-75 S',	'R',	'Dayton',	'Montgomery',	'OH',35.1,	9,	'Mostly Cloudy'],
-        ['A-4',	3,	'08-02-16 7:23',	'I-75 S',	'R',	'Dayton',	'Montgomery',	'OH',35.1,	9,	'Mostly Cloudy']
+        ['A-6',	3,	'08-02-16 7:44',	'Westerville Rd',	'R',	'Westerville',	'Franklin',	'OH',	37.9,	7,	'Light Rain'],
+        ['A-7',	2,	'08-02-16 7:59',	'N Woodward Ave',	'R',	'Dayton',	'Montgomery',	'OH',	34,	7,	'Overcast'],
+        ['A-8',	3,	'08-02-16 7:59',	'N Main St',	'R',	'Dayton',	'Montgomery',	'OH',	34,	7,	'Overcast'],
+        ['A-9',	2,	'08-02-16 8:00',	'Notre Dame Ave',	'L',	'Dayton',	'Montgomery',	'OH',	33.3,	5,	'Mostly Cloudy'],
+        ['A-10',	3,	'08-02-16 8:10',	'Westerville Rd',	'R',	'Westerville',	'Franklin',	'OH',	37.4,	3,	'Light Rain']
     ]);
     var cssClassNames = {
         'headerRow': 'cssHeaderRow',
