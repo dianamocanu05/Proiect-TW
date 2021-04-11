28cb48b045892bcfdd17d07d28d624bb0137e03a
olaruandreea12 <39705342+olaruandreea12@users.noreply.github.com>
2021-04-11 17:23:31 +0300

    table style

--- scripts/tabel.js
+++ scripts/tabel.js
@@ -19,10 +19,29 @@ function drawTable() {
         ['A-1',	3,	'08-02-16 5:46',	'I-70 E',	'R','Dayton',	'Montgomery', 'OH',	36.9,	10,	'Light Rain'],
         ['A-2',	2,	'08-02-16 6:07',	'Brice Rd',	'L', 'Reynoldsburg','Franklin',	'OH', 37.9 ,10,	'Light Rain'],
         ['A-3',	2,	'08-02-16 6:49',	'State Route 32',	'R',	'Williamsburg',	'Clermont',	'OH',	36,	10,	'Overcast'],
+        ['A-4',	3,	'08-02-16 7:23',	'I-75 S',	'R',	'Dayton',	'Montgomery',	'OH',35.1,	9,	'Mostly Cloudy'],
+        ['A-5',	2,	'08-02-16 7:39',	'Miamisburg Centerville Rd',	'R', 'Dayton',	'Montgomery',	'OH',	36,	6,	'Mostly Cloudy'],
+        ['A-4',	3,	'08-02-16 7:23',	'I-75 S',	'R',	'Dayton',	'Montgomery',	'OH',35.1,	9,	'Mostly Cloudy'],
+        ['A-4',	3,	'08-02-16 7:23',	'I-75 S',	'R',	'Dayton',	'Montgomery',	'OH',35.1,	9,	'Mostly Cloudy'],
+        ['A-4',	3,	'08-02-16 7:23',	'I-75 S',	'R',	'Dayton',	'Montgomery',	'OH',35.1,	9,	'Mostly Cloudy'],
+        ['A-4',	3,	'08-02-16 7:23',	'I-75 S',	'R',	'Dayton',	'Montgomery',	'OH',35.1,	9,	'Mostly Cloudy'],
         ['A-4',	3,	'08-02-16 7:23',	'I-75 S',	'R',	'Dayton',	'Montgomery',	'OH',35.1,	9,	'Mostly Cloudy']
     ]);
+    var cssClassNames = {
+        'headerRow': 'cssHeaderRow',
+        'tableRow': 'cssTableRow',
+        'oddTableRow': 'cssOddTableRow',
+        'selectedTableRow': 'cssSelectedTableRow',
+        'hoverTableRow': 'cssHoverTableRow',
+        'headerCell': 'cssHeaderCell',
+        'tableCell': 'cssTableCell',
+    };
 
+    var options = {
+        showRowNumber: false,
+        cssClassNames: cssClassNames
+    };
     var table = new google.visualization.Table(document.getElementById('table_div'));
 
-    table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+    table.draw(data, options);
 }
