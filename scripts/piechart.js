f0d4a4fcbc6f4778169c20feec48bf83e85682fd
AnaPricop <60388013+AnaPricop@users.noreply.github.com>
2021-04-11 09:15:31 +0300

    Update piechart.js

--- scripts/piechart.js
+++ scripts/piechart.js
@@ -1,32 +1,15 @@
-
-const sqlite3 = require("sqlite3").verbose();
-const path = require('path');
-
-const db = new sqlite3.Database('../data/mock/mock-db.sqlite3');
-let states= [];
-let number = [];
-let count = 0;
-   // let db = new sqlite3.Database(db_path);
-    db.all("SELECT state, count(*) as number FROM accidents GROUP BY state", function (err,rows){
-        if(err) return err;
-        rows.forEach(function (row){
-            states[count] = row.state;
-            number[count] = row.number;
-            count = count + 1;
-        });
-        db.close();
-    });
-         
- google.charts.load('current', {
+google.charts.load('current', {
   packages: ['corechart'],
   callback: drawChart
 });
-    
+
      function drawChart() {
 
         var data = google.visualization.arrayToDataTable([
         ['State', 'Number of accidents'],
-        [states[count-1], number[count-1]]
+        ['OH', 90],
+        ['MC', 15],
+        ['Fd', 42]
         ]);
 
         var options = {
@@ -37,4 +20,3 @@ let count = 0;
 
         chart.draw(data, options);
       }
-
