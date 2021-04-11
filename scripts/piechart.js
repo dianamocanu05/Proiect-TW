7347daf6eb5fdbccb19f80a3b7d2ccd46b46ef0f
AnaPricop <60388013+AnaPricop@users.noreply.github.com>
2021-04-11 09:13:06 +0300

    Update piechart.js

--- scripts/piechart.js
+++ scripts/piechart.js
@@ -1,41 +1,22 @@
-/*
-const sqlite3 = require("sqlite3");
-resolve = require('path').resolve
-*/
-//results = []
-//for row in cursor:
-//    results.append({'state': row[0], 'number': row[1]})
-  //  print "state :", row[0]
- //   print "number :", row[1]
-    
-    
-   // const sqlite3 = require('sqlite3').verbose();
 
-// open the database
-/*let db = new sqlite3.Database('data/mock/mock-db.sqlite3', sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the database.');
-});*/
+const sqlite3 = require("sqlite3").verbose();
+const path = require('path');
 
-/*db.serialize(() => {
-  db.each(`SELECT state, count(*) as number FROM accidents GROUP BY state`, (err, row) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log(row.id + "\t" + row.name);
-  });
-});
-
-db.close((err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
-*/
-    
+const db = new sqlite3.Database('../data/mock/mock-db.sqlite3');
+let states= [];
+let number = [];
+let count = 0;
+   // let db = new sqlite3.Database(db_path);
+    db.all("SELECT state, count(*) as number FROM accidents GROUP BY state", function (err,rows){
+        if(err) return err;
+        rows.forEach(function (row){
+            states[count] = row.state;
+            number[count] = row.number;
+            count = count + 1;
+        });
+        db.close();
+    });
+         
  google.charts.load('current', {
   packages: ['corechart'],
   callback: drawChart
@@ -45,9 +26,7 @@ db.close((err) => {
 
         var data = google.visualization.arrayToDataTable([
         ['State', 'Number of accidents'],
-        ['OH', 90],
-        ['MC', 15],
-        ['Fd', 42]
+        [states[count-1], number[count-1]]
         ]);
 
         var options = {
@@ -59,38 +38,3 @@ db.close((err) => {
         chart.draw(data, options);
       }
 
-/*function drawChart() {
-
-  var dataTable = new google.visualization.DataTable({
-      cols: [
-        { label: 'State', type: 'string' },
-        { label: 'Number', type: 'number' }
-      ]
-  });
-
-  // get html table rows
-  var results = document.getElementById('results');
-  Array.prototype.forEach.call(results.rows, function(row) {
-    // exclude column heading
-    if (row.rowIndex > 0) {
-      dataTable.addRow([
-        { v: (row.cells[0].textContent || row.cells[0].innerHTML).trim() },
-        { v: 1 }
-      ]);
-    }
-  });
-
-  var dataSummary = google.visualization.data.group(
-    dataTable,
-    [0],
-    [{'column': 1, 'aggregation': google.visualization.data.sum, 'type': 'number'}]
-  );
-
-  var options = {
-    title: 'My Daily Activities'
-  };
-
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-  chart.draw(dataSummary, options);
-}
-*/
